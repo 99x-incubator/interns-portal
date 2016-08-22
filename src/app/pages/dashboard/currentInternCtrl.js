059eb395f00d49b07d8ab010b16980e40f9167d1
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-08-22 13:56:22 +0530

    update dash bord chart

--- src/app/pages/dashboard/currentInternCtrl.js
+++ src/app/pages/dashboard/currentInternCtrl.js
@@ -14,6 +14,24 @@
   /** @ngInject */
   function CurrentInternCtrl($http) {
 
+    var container = document.getElementById('visualization');
+
+  // Create a DataSet (allows two way data-binding)
+  var items = new vis.DataSet([
+    {id: 1, content: 'item 1', start: '2013-04-20'},
+    {id: 2, content: 'item 2', start: '2013-04-14'},
+    {id: 3, content: 'item 3', start: '2013-04-18'},
+    {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
+    {id: 5, content: 'item 5', start: '2013-04-25'},
+    {id: 6, content: 'item 6', start: '2013-04-27'}
+  ]);
+
+  // Configuration for the Timeline
+  var options = {};
+
+  // Create a Timeline
+  var timeline = new vis.Timeline(container, items, options);
+
     var vm = this;
     vm.navigationCollapsed = true;
     vm.showCompose = function(subject, to , text){
@@ -27,6 +45,7 @@
 
 
 
+
     $http.get("https://58f6jw3pl0.execute-api.us-east-1.amazonaws.com/dev/")
     .then(function(response) {
         vm.tabs = response.data;
@@ -35,169 +54,6 @@
 
     });
 
-
-    /*vm.tabs = [
-      {
-        "id": "4563faass",
-        "name": "Nasta Linnie",
-        "subject": "Great text",
-        "date": "2015-08-28T07:57:09",
-        "pic": "img/Nasta.png",
-        "email": "petraramsey@mail.com",
-        "attachment": "poem.txt",
-        "position": "Great Employee",
-        "tag": "friend",
-        "labels": ['inbox']
-      },
-      {
-        "id": "4563fdfvd",
-        "name": "Nasta Linnie",
-        "subject": "Lores ipsum",
-        "date": "2015-11-19T03:30:45",
-        "important": false,
-        "pic": "img/Nasta.png",
-        "email": "petraramsey@mail.com",
-        "position": "Great Employee",
-        "tag": "study",
-        "labels": ['inbox']
-      },
-      {
-        "id": "4563zxcss",
-        "name": "Nasta Linnie",
-        "subject": "Lores ipsum",
-        "date": "2015-10-19T03:30:45",
-        "important": false,
-        "pic": "img/Nasta.png",
-        "email": "petraramsey@mail.com",
-        "position": "Great Employee",
-        "tag": "work",
-        "labels": ['sent', 'important']
-      },
-      {
-        "id": "8955sddf",
-        "name": "Nick Cat",
-        "subject": "New Design",
-        "date": "2015-05-05T12:59:45",
-        "pic": "img/Nick.png",
-        "email": "barlowshort@mail.com",
-        "position": "Graphical designer",
-        "attachment": "design.psd",
-        "tag": "work",
-        "labels": ['inbox']
-      },
-      {
-        "id": "8955sdfcc",
-        "name": "Nick Cat",
-        "subject": "Gift card",
-        "date": "2015-07-18T10:19:01",
-        "pic": "img/Nick.png",
-        "email": "barlowshort@mail.com",
-        "position": "Graphical designer",
-        "tag": "study",
-        "labels": ['inbox']
-      },
-      {
-        "id": "8955asewf",
-        "name": "Nick Cat",
-        "subject": "Some news",
-        "date": "2015-09-23T03:04:10",
-        "pic": "img/Nick.png",
-        "email": "barlowshort@mail.com",
-        "position": "Graphical designer",
-        "tag": "work",
-        "labels": ['inbox', 'important']
-      },
-      {
-        "id": "2334uudsa",
-        "name": "Kostya Danovsky",
-        "subject": "Street Art",
-        "date": "2015-11-22T10:05:09",
-        "pic": "img/Kostya.png",
-        "email": "schwart@mail.com",
-        "position": "Technical Chef",
-        "attachment": "file.doc",
-        "tag": "family",
-        "labels": ['inbox', 'important']
-      },
-      {
-        "id": "2334aefvv",
-        "name": "Kostya Danovsky",
-        "subject": "New product",
-        "date": "2015-06-22T06:26:10",
-        "pic": "img/Kostya.png",
-        "email": "schwart@mail.com",
-        "position": "Technical Chef",
-        "tag": "family",
-        "labels": ['inbox', 'important']
-      },
-      {
-        "id": "2334cvdss",
-        "name": "Kostya Danovsky",
-        "subject": "Old product",
-        "date": "2015-06-22T06:26:10",
-        "pic": "img/Kostya.png",
-        "email": "schwart@mail.com",
-        "position": "Technical Chef",
-        "tag": "study",
-        "labels": ['trash']
-      },
-      {
-        "id": "8223xzxfn",
-        "name": "Andrey Hrabouski",
-        "subject": "Skype moji",
-        "date": "2015-07-16T06:47:53",
-        "pic": "img/Andrey.png",
-        "email": "lakeishaphillips@mail.com",
-        "position": "Mobile Developer",
-        "tag": 'family',
-        "labels": ['trash']
-      },
-      {
-        "id": "8223sdffn",
-        "name": "Andrey Hrabouski",
-        "subject": "My App",
-        "date": "2015-06-20T07:05:02",
-        "pic": "img/Andrey.png",
-        "email": "lakeishaphillips@mail.com",
-        "position": "Mobile Developer",
-        "tag": 'family',
-        "labels": ['spam']
-      },
-      {
-        "id": "9391xdsff",
-        "name": "Vlad Lugovsky",
-        "subject": "Cool",
-        "date": "2015-03-31T11:52:58",
-        "pic": "img/Vlad.png",
-        "email": "carlsongoodman@mail.com",
-        "position": "Fullstack man",
-        "tag": "study",
-        "labels": ['draft']
-      },
-      {
-        "id": "8223xsdaa",
-        "name": "Andrey Hrabouski",
-        "subject": "Car rent",
-        "date": "2015-02-25T10:58:58",
-        "pic": "img/Andrey.png",
-        "email": "lakeishaphillips@mail.com",
-        "position": "Mobile Developer",
-        "tag": "family",
-        "labels": ['draft']
-      },
-      {
-        "id": "9391xdsff",
-        "name": "Vlad Lugovsky",
-        "subject": "What next",
-        "date": "2015-03-31T11:52:58",
-        "pic": "img/Vlad.png",
-        "email": "carlsongoodman@mail.com",
-        "position": "Fullstack man",
-        "tag": "study",
-        "labels": ['sent']
-      }
-    ];
-    */
   }
 
 
