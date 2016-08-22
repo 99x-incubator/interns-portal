c0ad6e4cf3cd3368a3dd6fc96a674495e2939f0f
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-08-22 15:48:51 +0530

    time line is working now

--- src/app/pages/dashboard/currentInternCtrl.js
+++ src/app/pages/dashboard/currentInternCtrl.js
@@ -14,24 +14,6 @@
   /** @ngInject */
   function CurrentInternCtrl($http) {
 
-    var container = document.getElementById('visualization');
-
-  // Create a DataSet (allows two way data-binding)
-  var items = new vis.DataSet([
-    {id: 1, content: 'item 1', start: '2013-04-20'},
-    {id: 2, content: 'item 2', start: '2013-04-14'},
-    {id: 3, content: 'item 3', start: '2013-04-18'},
-    {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-    {id: 5, content: 'item 5', start: '2013-04-25'},
-    {id: 6, content: 'item 6', start: '2013-04-27'}
-  ]);
-
-  // Configuration for the Timeline
-  var options = {};
-
-  // Create a Timeline
-  var timeline = new vis.Timeline(container, items, options);
-
     var vm = this;
     vm.navigationCollapsed = true;
     vm.showCompose = function(subject, to , text){
@@ -46,15 +28,38 @@
 
 
 
+
     $http.get("https://58f6jw3pl0.execute-api.us-east-1.amazonaws.com/dev/")
     .then(function(response) {
         vm.tabs = response.data;
-
-        console.log(vm.tabs);
+        internsTimeline(vm.tabs);
 
     });
 
-  }
+  };
+
+  function internsTimeline(interns){
+    //console.log(interns);
+
+    var container = document.getElementById('visualization');
+    var data=[];
+    angular.forEach(interns.records, function(item){
+        data.push({id: item.id, content: item.firstname, start: item.startdate, end:item.enddate});
+                   console.log(item.firstname);
+               })
+
+
+   // Create a DataSet (allows two way data-binding)
+   var items = new vis.DataSet(data);
+
+   // Configuration for the Timeline
+   var options = {};
+
+   // Create a Timeline
+   var timeline = new vis.Timeline(container, items, options);
+
+
+  };
 
 
 })();
