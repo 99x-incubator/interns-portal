922c99af5502be8e004909b4f80ee6f90d9b3531
pmartz92 <pmartz92@gmail.com>
2016-09-12 17:26:19 +0530

    task notification added

--- web/src/app/pages/tasks/viewtasks.controller.js
+++ web/src/app/pages/tasks/viewtasks.controller.js
@@ -4,11 +4,31 @@
     angular.module('BlurAdmin.pages.viewtasks').controller('ViewTasksPageCtrl', ViewTasksPageCtrl);
 
 
-    function ViewTasksPageCtrl($scope, $timeout, $http, AuthenticationService) {
+    function ViewTasksPageCtrl($scope, $timeout, $http, AuthenticationService, toastr, toastrConfig) {
+
+        var defaultConfig = angular.copy(toastrConfig);
+        var openedToasts = [];
+        $scope.options = {
+            autoDismiss: false,
+            positionClass: 'toast-top-right',
+            type: 'success',
+            timeOut: '5000',
+            extendedTimeOut: '2000',
+            allowHtml: false,
+            closeButton: true,
+            tapToDismiss: true,
+            progressBar: false,
+            newestOnTop: true,
+            maxOpened: 0,
+            preventDuplicates: false,
+            preventOpenDuplicates: false,
+            title: "",
+            msg: "My plan updated"
+        };
 
         /* Get tasks list from API, these are the tast that currently available*/
 
-        $http.get('https://04z6zajmp1.execute-api.us-east-1.amazonaws.com/dev/allTask').then(function tasksList(response) {
+        $http.get('https://ezh9ingj6l.execute-api.us-east-1.amazonaws.com/dev/allTask').then(function tasksList(response) {
             $scope.list1 = response.data.data.Items;
             //print($scope.list6);
         });
@@ -21,50 +41,47 @@
 
 
         $scope.list4 = [];
-        var dd = {
-            "id": "test1@gmail.com",
-            "task": $scope.list4
-        };
 
 
 
+        //add user task function, from this new tasks will add to the plan
         var addUserTasks = function() {
-            $http.post('https://04z6zajmp1.execute-api.us-east-1.amazonaws.com/dev/updateUserTask', dd).then(function(response) {
+            var newTask = {
+                "id": "test1@gmail.com",
+                "task": $scope.list4
+            };
+
+            $http.post('https://ezh9ingj6l.execute-api.us-east-1.amazonaws.com/dev/updateUserTask', newTask).then(function(response) {
                 if (response.data.status == "success") {
+                    angular.extend(toastrConfig, $scope.options);
+                    openedToasts.push(toastr[$scope.options.type]($scope.options.msg, $scope.options.title));
+                    var strOptions = {};
+                    for (var o in $scope.options)
+                        if (o != 'msg' && o != 'title') strOptions[o] = $scope.options[o];
                     print("task added");
-                    //getUserCurrenTasks();
                 } else {
-                    print("error");
+                    $scope.options['msg'] = "Error in adding task";
+                    $scope.options['type'] = "error";
+                    angular.extend(toastrConfig, $scope.options);
+                    openedToasts.push(toastr[$scope.options.type]($scope.options.msg, $scope.options.title));
+                    var strOptions = {};
+                    for (var o in $scope.options)
+                        if (o != 'msg' && o != 'title') strOptions[o] = $scope.options[o];
                 }
             });
         };
 
         var getUserCurrenTasks = function() {
-            $http.post('https://04z6zajmp1.execute-api.us-east-1.amazonaws.com/dev/getUserTask', userID).then(function(response) {
+            $http.post('https://ezh9ingj6l.execute-api.us-east-1.amazonaws.com/dev/getUserTask', userID).then(function(response) {
                 if (response.data.status == "success") {
-                    $scope.list4 =response.data.data.Item.task;
-                    //print(JSON.stringify(response.data.data.Item.task));
+                    $scope.list4 = response.data.data.Item.task;
                     print("Tasks retrieved");
-                    print($scope.list4);
                 } else if (response.data.status == "error") {
                     print("User authentication error");
                 }
             });
         };
         getUserCurrenTasks();
-
-        // $scope.list1 = [{
-        //     'id': 'Node'
-        // }, {
-        //     'id': 'Angular'
-        // }, {
-        //     'id': 'React'
-        // }, {
-        //     'id': 'Bootstrap'
-        // }, {
-        //     'id': 'Leadership'
-        // }];
-
 
         $scope.hideMe = function() {
             return $scope.list4.length > 0;
@@ -77,7 +94,6 @@
 
         //Actions for save button
         $scope.savetasks = function() {
-            print(dd);
             addUserTasks();
         };
 
