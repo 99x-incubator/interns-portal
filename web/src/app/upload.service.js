06de6180cd11cb22b5411a45c7ccbb0bfe2f9a4d
nadunindunil <nadun1indunil@gmail.com>
2016-09-26 09:57:49 +0530

    upload was changed

--- web/src/app/upload.service.js
+++ web/src/app/upload.service.js
@@ -5,7 +5,7 @@
         .service('S3UploadService', ['$q', function($q) {
             // Us standard region
             AWS.config.region = 'us-east-1';
-            AWS.config.update({ accessKeyId: 'AKIAJQFZMNLUKZXHGGGQ', secretAccessKey: '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' });
+            AWS.config.update({ accessKeyId: '', secretAccessKey: '' });
 
             var bucket = new AWS.S3({ params: { Bucket: '99xt-interns-uploads', maxRetries: 10 }, httpOptions: { timeout: 360000 } });
 
